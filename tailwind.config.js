b1b9686fed45a3c80ed8d02618e5de8602c0fe58
Kazox6 <pawelka00florian@gmail.com>
2025-01-10 09:13:38 +0100

    Adds colors to tailwind config and use them.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,55 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_text: {
+          light: '#081d2b',
+          DEFAULT: '#02070a',
+          dark: '#010304'
+        },
+        primary_background: {
+          light: '#fbfdfe',
+          DEFAULT: '#f5fafd',
+          dark: '#ddeef8'
+        },
+        primary: {
+          light: '#60b6e1',
+          DEFAULT: '#39a6db',
+          dark: '#2c8ab8'
+        },
+        secondary: {
+          light: '#f4c2c5',    // Lighter pink
+          DEFAULT: '#eb8e92',  // Your current default
+          dark: '#e35f65'      // Darker pink
+        },
+        accent: {
+          light: '#e8d178',    // Lighter yellow
+          DEFAULT: '#e0c454',  // Your current default
+          dark: '#c7ac3d'      // Darker yellow
+        }
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.primary_text.DEFAULT'),
+            h1: {
+              color: theme('colors.primary_text.DEFAULT'),
+            },
+            h2: {
+              color: theme('colors.primary_text.DEFAULT'),
+            },
+            h3: {
+              color: theme('colors.primary_text.DEFAULT'),
+            },
+            h4: {
+              color: theme('colors.primary_text.DEFAULT'),
+            }
+
+          }
+        }
+      })
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
