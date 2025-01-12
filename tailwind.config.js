68d22f75b1b46e7f830c99f7fd1f44a1f13b0481
Kazox6 <pawelka00florian@gmail.com>
2025-01-12 22:14:40 +0100

    Updates Form Layout.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.md'],
+  content: ['./layouts/**/*.html', './content/**/*.{md,html}'],
   theme: {
     extend: {
       colors: {
