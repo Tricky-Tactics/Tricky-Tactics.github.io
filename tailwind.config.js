4dd3b7ba598ab26dd60b59de9cbfd4a9c703bd8f
Kazox61 <kazox61@gmail.com>
2025-09-04 16:27:05 +0200

    Update Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,57 @@
 module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.{md,html}'],
-  theme: {
-    extend: {
-      colors: {
-        primary_text: {
-          light: '#081d2b',
-          DEFAULT: '#02070a',
-          dark: '#010304'
-        },
-        primary_background: {
-          light: '#fbfdfe',
-          DEFAULT: '#f5fafd',
-          dark: '#ddeef8'
-        },
-        primary: {
-          light: '#60b6e1',
-          DEFAULT: '#39a6db',
-          dark: '#2c8ab8'
-        },
-        secondary: {
-          light: '#f4c2c5',    // Lighter pink
-          DEFAULT: '#eb8e92',  // Your current default
-          dark: '#e35f65'      // Darker pink
-        },
-        accent: {
-          light: '#e8d178',    // Lighter yellow
-          DEFAULT: '#e0c454',  // Your current default
-          dark: '#c7ac3d'      // Darker yellow
-        }
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            maxWidth: '100ch',
-            color: theme('colors.primary_text.DEFAULT'),
-            h1: {
-              color: theme('colors.primary_text.DEFAULT'),
+    content: ['./layouts/**/*.html', './content/**/*.{md,html}'],
+    theme: {
+        extend: {
+            colors: {
+                background: {
+                    light: '#ffffff',
+                    DEFAULT: '#e6e6e6',
+                    dark: '#c0c0c0',
+                },
+                on_everything: {
+                    light: '#2d2d2d',
+                    DEFAULT: '#0f0f0f',
+                    dark: '#000000',
+                },
+                primary: {
+                    light: '#659cf6',
+                    DEFAULT: '#3b82f6',
+                    dark: '#1f6ae2'
+                },
+                secondary: {
+                    light: '#f4c2c5',
+                    DEFAULT: '#eb8e92',
+                    dark: '#e35f65'
+                },
+                accent: {
+                    light: '#e8d178',
+                    DEFAULT: '#e0c454',
+                    dark: '#c7ac3d'
+                }
             },
-            h2: {
-              color: theme('colors.primary_text.DEFAULT'),
-            },
-            h3: {
-              color: theme('colors.primary_text.DEFAULT'),
-            },
-            h4: {
-              color: theme('colors.primary_text.DEFAULT'),
-            }
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        maxWidth: '100ch',
+                        color: theme('colors.primary_text.DEFAULT'),
+                        h1: {
+                            color: theme('colors.primary_text.DEFAULT'),
+                        },
+                        h2: {
+                            color: theme('colors.primary_text.DEFAULT'),
+                        },
+                        h3: {
+                            color: theme('colors.primary_text.DEFAULT'),
+                        },
+                        h4: {
+                            color: theme('colors.primary_text.DEFAULT'),
+                        }
 
-          }
-        }
-      })
+                    }
+                }
+            })
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require("@tailwindcss/typography")],
 }
 
